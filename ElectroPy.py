99d12026555c7c1a1b924e293c787c27414b81a3
Elijah Barrett <elijahbarrett22@gmail.com>
2019-06-24 08:08:58 -0500

    added getDotProduct function

--- ElectroPy.py
+++ ElectroPy.py
@@ -1,5 +1,5 @@
 
-"""
+'''
 This is the main package/library for this project. Eventually, it will hold all the functions necessary for
 solving multiple types of problems in the area of electromagetics.
 
@@ -7,22 +7,32 @@ solving multiple types of problems in the area of electromagetics.
     Functions needed:
         - Partial Derivatives
         - Curl
+        - Cross Product
     Currently working on:
         - Partial Derivatives (getPartialDerivative)
             - Figuring out where to put/ how to figure out symbols
-"""
+'''
 
 from sympy import *
 import numpy as np
 
 def getDerivative(f, symbol):
+    ''' Return the derivative of the function f with respect to symbol. '''
     f_prime = y.diff(symbol)
     print(f_prime)
+    return f_prime
 
 def getPartialDerivative(f):
+    ''' Return the partial derivative of the function f with respect to symbol. '''
     x, y, z = symbols('x y z', real=True)
     f_prime = diff(f, x)
     print(f_prime)
+    return f_prime
+
+def getDotProduct(v1, v2):
+    '''Return the dot product of two equal-length vectors. '''
+    print('Dot product of the two vectors: ' + str(np.dot(v1, v2)))
+    return(np.dot(v1, v2))
 
 # Testing the getDerivative function
 x = Symbol('x')
@@ -33,3 +43,8 @@ getDerivative(y, x)
 x, y, z = symbols('x y z', real=True)
 f = 4*x*y + x*sin(z) + x**3 + z**8*y
 getPartialDerivative(f)
+
+# Testing the getDotProduct functions
+E = np.array([1, 2, 3])
+B = np.array([1, 2, 3])
+scalar = getDotProduct(E, B)
