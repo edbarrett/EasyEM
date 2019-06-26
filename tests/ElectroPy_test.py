9e2ba048936c4d62e4305faf6bb3523143671edc
Elijah Barrett <elijahbarrett22@gmail.com>
2019-06-26 08:39:57 -0500

    Created a base unit test file.

---
+++ tests/ElectroPy_test.py
@@ -0,0 +1,14 @@
+'''This is the unit test suite for ElectroPy.py.
+
+Test naming convention: <method>_Should<expected>_When<condition>
+'''
+
+import unittest
+import ElectroPy
+
+class ElectroPyFuncTests(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
