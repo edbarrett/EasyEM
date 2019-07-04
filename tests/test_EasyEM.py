3e4c66386618f71bd3938cd607f3704b17d27d31
Elijah Barrett <elijahbarrett22@gmail.com>
2019-07-04 08:58:04 -0500

    Added tests for get_derivative for all coordinate systems.

--- tests/test_EasyEM.py
+++ tests/test_EasyEM.py
@@ -1,22 +1,51 @@
 '''This is the unit test suite for ElectroPy.py.
 
-Test naming convention: <method>_Should<expected>_When<condition>
+Need to set up as a package first.
 '''
 
 import unittest
-from ElectroPy import *
+import EasyEM as em
 from sympy.abc import x, y, z, theta, rho, phi
+from sympy import Symbol
+radi = Symbol('radi')
 
-class ElectroPyFuncTests(unittest.TestCase):
+class EasyEMFuncTests(unittest.TestCase):
 
-    def test_getDerivative(self):
-        f = x
-        self.assertEqual(getDerivative(f, x), 1)
+    def test_get_derivative_cartesian_x(self):
+        function = x + y + z**2
+        self.assertEqual(em.get_derivative(function, x), 1)
 
-    def test_getDerivative(self):
-        f = x**2
-        self.assertEqual(getDerivative(f, x), 2*x)
+    def test_get_derivative_cartesian_y(self):
+        function = x + y + z**2
+        self.assertEqual(em.get_derivative(function, y), 1)
 
+    def test_get_derivative_cartesian_z(self):
+        function = x + y + z**2
+        self.assertEqual(em.get_derivative(function, z), 2*z)
+
+    def test_get_derivative_cylindrical_rho(self):
+        function = rho + phi + z**2
+        self.assertEqual(em.get_derivative(function, rho), 1)
+
+    def test_get_derivative_cylindrical_phi(self):
+        function = rho + phi + z**2
+        self.assertEqual(em.get_derivative(function, phi), 1)
+
+    def test_get_derivative_cylindrical_z(self):
+        function = rho + phi + z**2
+        self.assertEqual(em.get_derivative(function, z), 2*z)
+
+    def test_get_derivative_spherical_radi(self):
+        function = radi + theta + phi**2
+        self.assertEqual(em.get_derivative(function, radi), 1)
+
+    def test_get_derivative_spherical_theta(self):
+        function = radi + theta + phi**2
+        self.assertEqual(em.get_derivative(function, theta), 1)
+
+    def test_get_derivative_spherical_phi(self):
+        function = radi + theta + phi**2
+        self.assertEqual(em.get_derivative(function, phi), 2*phi)
 
 if __name__ == '__main__':
     unittest.main()
